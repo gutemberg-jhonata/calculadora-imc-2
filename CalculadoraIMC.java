256bdbd62b60ace75b2530c2efcf0b288572a495
Jhonata <gutemberg.jhonata@gmail.com>
2023-07-05 11:15:04 -0300

    refatorando código com constantes globais

--- CalculadoraIMC.java
+++ CalculadoraIMC.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 
 class CalculadoraIMC {
+    static final double NO_PESO_LIMITE_INFERIOR_FEMININO = 19.1;
+    static final double NO_PESO_LIMITE_SUPERIOR_FEMININO = 25.8;
+    static final double UM_POUCO_ACIMA_PESO_LIMITE_INFERIOR_FEMININO = 25.9;
+    static final double UM_POUCO_ACIMA_PESO_LIMITE_SUPERIOR_FEMININO = 27.3;
+    static final double ACIMA_PESO_LIMITE_INFERIOR_FEMININO = 27.4;
+    static final double ACIMA_PESO_LIMITE_SUPERIOR_FEMININO = 32.3;
+
+    static final double NO_PESO_LIMITE_INFERIOR_MASCULINO = 20.8;
+    static final double NO_PESO_LIMITE_SUPERIOR_MASCULINO = 26.4;
+    static final double UM_POUCO_ACIMA_PESO_LIMITE_INFERIOR_MASCULINO = 26.5;
+    static final double UM_POUCO_ACIMA_PESO_LIMITE_SUPERIOR_MASCULINO = 27.8;
+    static final double ACIMA_PESO_LIMITE_INFERIOR_MASCULINO = 27.9;
+    static final double ACIMA_PESO_LIMITE_SUPERIOR_MASCULINO = 31.1;
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite seu sexo (M ou F): ");
@@ -12,27 +26,27 @@ class CalculadoraIMC {
         scanner.close();
 
         final double imc = peso/(altura*altura);
-       
+
         if (sexo == "F") {
-            if (imc < 19.1) {
+            if (imc < NO_PESO_LIMITE_INFERIOR_FEMININO) {
                 System.out.println("Abaixo do peso");
-            } else if (imc >= 19.1 && imc <= 25.8) {
+            } else if (imc >= NO_PESO_LIMITE_INFERIOR_FEMININO && imc <= NO_PESO_LIMITE_SUPERIOR_FEMININO) {
                 System.out.println("No peso ideal");
-            } else if (imc >= 25.9 && imc <= 27.3) {
+            } else if (imc >= UM_POUCO_ACIMA_PESO_LIMITE_INFERIOR_FEMININO && imc <= UM_POUCO_ACIMA_PESO_LIMITE_SUPERIOR_FEMININO) {
                 System.out.println("Um pouco acima do peso");
-            } else if (imc >= 27.4 && imc <= 32.3) {
+            } else if (imc >= ACIMA_PESO_LIMITE_INFERIOR_FEMININO && imc <= ACIMA_PESO_LIMITE_SUPERIOR_FEMININO) {
                 System.out.println("Acima do peso ideal");
             } else {
                 System.out.println("Obeso");
             }
         } else {
-            if (imc < 20.7) {
+            if (imc < NO_PESO_LIMITE_INFERIOR_MASCULINO) {
                 System.out.println("Abaixo do peso");
-            } else if (imc >= 20.8 && imc <= 26.4) {
+            } else if (imc >= NO_PESO_LIMITE_INFERIOR_MASCULINO && imc <= NO_PESO_LIMITE_SUPERIOR_MASCULINO) {
                 System.out.println("No peso ideal");
-            } else if (imc >= 26.5 && imc <= 27.8) {
+            } else if (imc >= UM_POUCO_ACIMA_PESO_LIMITE_INFERIOR_MASCULINO && imc <= UM_POUCO_ACIMA_PESO_LIMITE_SUPERIOR_MASCULINO) {
                 System.out.println("Um pouco acima do peso");
-            } else if (imc >= 27.9 && imc <= 31.1) {
+            } else if (imc >= ACIMA_PESO_LIMITE_INFERIOR_MASCULINO && imc <= ACIMA_PESO_LIMITE_SUPERIOR_MASCULINO) {
                 System.out.println("Acima do peso ideal");
             } else {
                 System.out.println("Obeso");
